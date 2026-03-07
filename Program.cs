77c29756e90722a1ff2e42c322fe2616485c0c2e
Vyacheslav <relaxerrkis@gmail.com>
2026-03-07 13:35:52 +0400

    Added packages, added sln, added basic code to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+// using MyDndBot.Data;
+// using MyDndBot.Telegram;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// 1. Регистрация настроек (токен и БД)
+// В реальности лучше брать из builder.Configuration
+string botToken = "ВАШ_ТОКЕН";
+
+// 2. Подключаем Telegram Bot Client
+builder.Services.AddSingleton(provider =>
+    new TelegramBotClient(botToken));
+
+// 3. Подключаем Базу Данных
+// builder.Services.AddDbContext();
+
+// 4. Регистрация наших сервисов
+// builder.Services.AddScoped(); // Логика обработки сообщений
+// builder.Services.AddHostedService(); // Сервис, который держит бота запущенным
+// builder.Services.AddHostedService(); // Тот самый сервис для напоминаний раз в 3 часа
+
+using IHost host = builder.Build();
+
+// Автоматическое применение миграций при старте (удобно для разработки)
+using (var scope = host.Services.CreateScope())
+{
+    // var db = scope.ServiceProvider.GetRequiredService();
+    // db.Database.EnsureCreated();
+}
+
+await host.RunAsync();
